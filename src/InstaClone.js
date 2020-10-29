4c1cef96f163095dbb7d0e026d33285061b8f36f
calvinhlee23@gmail.com <calvinhlee23@gmail.com>
2020-10-29 00:12:12 -0700

    first commit

---
+++ src/InstaClone.js
@@ -0,0 +1,13 @@
+import './InstaClone.css';
+
+function InstaClone() {
+  return (
+    <div className="InstaClone">
+      <header className="InstaClone-header">
+        <text>Hi this is InstaClone Root I'll add routing logic later</text>
+      </header>
+    </div>
+  );
+}
+
+export default InstaClone;
